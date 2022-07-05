ddbca40a832539f1428bd257c97370e7f9e1c79e
leaf222 <ahtlye@163.com>
2022-07-05 21:19:29 +0100

    project architecture

---
+++ gulimail-testmodule/src/main/java/com/leaf/gulimail/testmodule/GulimailTestmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.leaf.gulimail.testmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimailTestmoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimailTestmoduleApplication.class, args);
+    }
+
+}
---
+++ gulimail-testmodule/src/test/java/com/leaf/gulimail/testmodule/GulimailTestmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaf.gulimail.testmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailTestmoduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
